90737701cb30016288b5c95137a67ed4249b9f11
Manoj Mahato <127175133+Manoj-Mahat0@users.noreply.github.com>
2024-10-21 07:31:39 +0530

    Update fact.sol

--- fact.sol
+++ fact.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract Factorial {
-    // Function to calculate the factorial of a number
+
     function factorial(uint num) public pure returns (uint) {
         uint f = 1;
         for (uint i = 1; i <= num; i++) {
